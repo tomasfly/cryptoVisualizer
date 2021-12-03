cd620002858054e532c8ae5476cd07457f12a806
Tomas Fleiderman <tomas.fleiderman@gmail.com>
2021-12-03 19:04:51 +0100

    update branch

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -34,35 +34,35 @@ class Bootstrap {
         //     let volumeInfo = volume.getVolume(response,coin,timeframe)
         //     console.log(volumeInfo)
         // });
-        while (true) {
-            console.log("Starting to retrieve coins volume at " + new Date().toISOString() + `with params timeframe: ${this.timeframe} candlesNumber:${this.candlesNumber}`)
-            for (let index = 0; index < this.usdtList.length; index++) {
-                let volume = new Volume()
-                let volumeInfo
-                let response = await TaapiExchange.getCandlesAsync(this.usdtList[index], this.timeframe, this.candlesNumber)
-                if (response) {
-                    volumeInfo = volume.getVolume(response, this.usdtList[index], this.timeframe)
-                }
-                //log Volume only if actual volume is higher compared with the average of the last {candlesNumber}
-                if (volumeInfo) {
-                    if (volumeInfo.isVolumeHigher) {
-                        // logic to print different colors
-                        if (volumeInfo.percentageIncrement > 250.0 && volumeInfo.percentageIncrement < 500.0) {
-                            console.log(`[INFO ${new Date().toISOString()}] VALUE LARGER THAN 250 for ${volumeInfo.coin} with an incremental percentage value of ${volumeInfo.percentageIncrement}${this.params}`.green)
-                        }
+        // while (true) {
+        console.log("Starting to retrieve coins volume at " + new Date().toISOString() + `with params timeframe: ${this.timeframe} candlesNumber:${this.candlesNumber}`)
+        for (let index = 0; index < this.usdtList.length; index++) {
+            let volume = new Volume()
+            let volumeInfo
+            let response = await TaapiExchange.getCandlesAsync(this.usdtList[index], this.timeframe, this.candlesNumber)
+            if (response) {
+                volumeInfo = volume.getVolume(response, this.usdtList[index], this.timeframe)
+            }
+            //log Volume only if actual volume is higher compared with the average of the last {candlesNumber}
+            if (volumeInfo) {
+                if (volumeInfo.isVolumeHigher) {
+                    // logic to print different colors
+                    if (volumeInfo.percentageIncrement > 250.0 && volumeInfo.percentageIncrement < 500.0) {
+                        console.log(`[INFO ${new Date().toISOString()}] VALUE LARGER THAN 250 for ${volumeInfo.coin} with an incremental percentage value of ${volumeInfo.percentageIncrement}${this.params}`.green)
+                    }
 
-                        if (volumeInfo.percentageIncrement > 500.0 && volumeInfo.percentageIncrement < 1000.0) {
-                            console.log(`[INFO ${new Date().toISOString()}] VALUE LARGER THAN 500 for ${volumeInfo.coin} with an incremental percentage value of ${volumeInfo.percentageIncrement}${this.params}`.yellow)
-                        }
+                    if (volumeInfo.percentageIncrement > 500.0 && volumeInfo.percentageIncrement < 1000.0) {
+                        console.log(`[INFO ${new Date().toISOString()}] VALUE LARGER THAN 500 for ${volumeInfo.coin} with an incremental percentage value of ${volumeInfo.percentageIncrement}${this.params}`.yellow)
+                    }
 
-                        if (volumeInfo.percentageIncrement > 1000.0 && volumeInfo.percentageIncrement) {
-                            console.log(`[INFO ${new Date().toISOString()}] VALUE LARGER THAN 1000 ${volumeInfo.coin} with an incremental percentage value of ${volumeInfo.percentageIncrement}${this.params}`.red)
-                        }
+                    if (volumeInfo.percentageIncrement > 1000.0 && volumeInfo.percentageIncrement) {
+                        console.log(`[INFO ${new Date().toISOString()}] VALUE LARGER THAN 1000 ${volumeInfo.coin} with an incremental percentage value of ${volumeInfo.percentageIncrement}${this.params}`.red)
                     }
                 }
             }
-            console.log("Done retrieveing coins volume at " + new Date().toISOString())
         }
+        console.log("Done retrieveing coins volume at " + new Date().toISOString())
+        // }
     }
 }
 
